2e40343bf178bb5a9a5aa3bd70ff3843593fba32
yafei06.zhang <yafei06.zhang@vipshop.com>
2022-08-08 21:14:56 +0800

    [Refactor] refactor Vector

--- include/merlin_hashtable.cuh
+++ include/merlin_hashtable.cuh
@@ -20,6 +20,7 @@
 #include <thrust/execution_policy.h>
 #include <thrust/sort.h>
 #include <mutex>
+#include <type_traits>
 #include "merlin/core_kernels.cuh"
 #include "merlin/initializers.cuh"
 #include "merlin/utils.cuh"
@@ -27,20 +28,20 @@
 namespace nv {
 namespace merlin {
 
+// TODO(kimi): why not use std::array<T, D>?
 /**
- * The basic value type in Merlin-KV.
- * Any user data should be represented as a specific type of Vector in order to
- * be processed by Merlin-KV.
+ * @brief value type of Merlin-KV.
  *
- * @tparam value_type type of the Vector's elements type, which should be basic
- * types of C++/CUDA.
+ * @tparam T type of elements of the vector. It must be POD.
  * @tparam D dimension of the vector.
- *
  */
-template <class value_type, size_t D>
+template <typename T, size_t D>
 struct Vector {
+  static_assert(std::is_pod<T>::value, "T must be POD.");
+  using value_type = T;
   static constexpr size_t DIM = D;
-  value_type value[D];
+  // TODO(kimi): rename value to values?
+  value_type value[DIM];
 };
 
 /**
