e8cad7d5f1b703118385c9d581ebb9d8ca3a5e13
Lifann <xhlyfan@gmail.com>
2022-10-17 21:35:21 +0800

    localfile(comments): Add comments to explain open and close bahaviors

--- include/merlin_localfile.h
+++ include/merlin_localfile.h
@@ -30,7 +30,9 @@ namespace merlin {
  * values of metas are commonly time-variant, while the time interval
  * between save/load calling is not deterministic, in default case. If
  * other specified rules are required, the BaseKVFile could be inherited
- * to implement customized read/write rules.
+ * to implement customized read/write rules. The LocalKVFile uses compact,
+ * consecutive binary format, where keys, values, and metas are stored in
+ * seperated paths.
  *
  * @tparam K The data type of the key.
  * @tparam V The data type of the vector's elements.
@@ -47,6 +49,16 @@ class LocalKVFile : public BaseKVFile<K, V, M, D> {
 
   ~LocalKVFile() { close(); }
 
+  /**
+   * @brief Open the file from local path. A LocalKVFile can only be
+   * read or written when it stays opened.
+   *
+   * @param keys_path Path to file to store keys.
+   * @param values_path Path to file to store values.
+   * @param metas_path Path to file to store metas.
+   * @params mode The mode to the file. The mode follows glibc style
+   *              and behavior like fopen.
+   */
   bool open(const std::string& keys_path, const std::string& values_path,
             const std::string& metas_path, const char* mode) {
     close();
@@ -67,6 +79,10 @@ class LocalKVFile : public BaseKVFile<K, V, M, D> {
     return true;
   }
 
+  /**
+   * @brief Close the file from open status and release fd(s) on files
+   * of keys, values, and metas.
+   */
   void close() noexcept {
     if (keys_fp_) {
       fclose(keys_fp_);
@@ -82,6 +98,20 @@ class LocalKVFile : public BaseKVFile<K, V, M, D> {
     }
   }
 
+  /**
+   * Read from file and fill into the keys, values, and metas buffer.
+   * When calling save/load method from table, it can assume that the
+   * received buffer of keys, vectors, and metas are automatically
+   * pre-allocated.
+   *
+   * @param n The number of KV pairs expect to read. `int64_t` was used
+   *          here to adapt to various filesytem and formats.
+   * @param keys The pointer to received buffer for keys.
+   * @param vectors The pointer to received buffer for vectors.
+   * @param metas The pointer to received buffer for metas.
+   *
+   * @return Number of KV pairs have been successfully read.
+   */
   size_t read(size_t n, K* keys, V* vectors, M* metas) override {
     size_t nread_keys =
         fread(keys, sizeof(K), static_cast<size_t>(n), keys_fp_);
@@ -95,6 +125,17 @@ class LocalKVFile : public BaseKVFile<K, V, M, D> {
     return nread_keys;
   }
 
+  /**
+   * Write keys, values, metas from table to the file.
+   *
+   * @param n The number of KV pairs to be written. `int64_t` was used
+   *          here to adapt to various filesytem and formats.
+   * @param keys The keys will be written to file.
+   * @param vectors The vectors of values will be written to file.
+   * @param metas The metas will be written to file.
+   *
+   * @return Number of KV pairs have been successfully written.
+   */
   size_t write(size_t n, const K* keys, const V* vectors,
                const M* metas) override {
     size_t nwritten_keys =
