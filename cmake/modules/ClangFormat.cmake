b871c51ac3f7eaca4e358d829fae58c6bebacf6d
Matthias Langer <bashimao@users.noreply.github.com>
2022-11-16 04:58:32 -0800

    Allow optional clang format of source files (off by default).

--- cmake/modules/ClangFormat.cmake
+++ cmake/modules/ClangFormat.cmake
@@ -28,7 +28,7 @@ function(clangformat_setup clangformat_srcs)
 
   add_custom_target(${PROJECT_NAME}_clangformat ALL
                     COMMAND ${CLANGFORMAT_EXECUTABLE}
-                            -style=google
+                            -style=file
                             -i
                             ${clangformat_srcs}
                     COMMENT "Formating with ${CLANGFORMAT_EXECUTABLE} ...")
