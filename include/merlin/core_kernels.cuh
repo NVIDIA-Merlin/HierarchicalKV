e2c782863158767f457913680191cf28c3543bad
rhdong <rhdong2017@gmail.com>
2023-02-21 11:10:38 +0800

    [Opt] decrease the slice size to 1GB

--- include/merlin/core_kernels.cuh
+++ include/merlin/core_kernels.cuh
@@ -111,8 +111,8 @@ __global__ void create_atomic_metas(Bucket<K, V, M>* __restrict buckets,
   }
 }
 
-/* 2GB per slice by default.*/
-constexpr size_t kDefaultBytesPerSlice = (8ul << 30);
+/* 1GB per slice by default.*/
+constexpr size_t kDefaultBytesPerSlice = (1ul << 30);
 
 /* Initialize the buckets with index from start to end. */
 template <class K, class V, class M>
