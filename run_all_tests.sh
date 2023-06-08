ba689ff6bd1def45c55e42e35d07f4cd982468b3
rhdong <rhdong2017@gmail.com>
2023-06-08 18:44:46 +0800

    [CI] just check CI work as expected

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
+# Usage : `bash run_all_tests.sh`
+
 # Search for all binary files that end with "test"
 files=$(find ./build/ -type f -name "*_test" -executable)
 
 # Execute each file found
+has_fail=false
 for file in $files
 do
     echo "Executing $file ..."
     ./$file
+    if ! [ $? -eq 0 ]; then
+      has_fail=true
+    fi
 done
+
+if [ "$has_fail" = true ] ; then
+    exit 1
+fi
